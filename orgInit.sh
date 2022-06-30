9a95b009e0b50c3fc821f3d93c0760af2521986b
sghanta-sf <sumanth.ghanta@salesforce.com>
2022-06-29 19:25:16 -0700

    updated permset

--- orgInit.sh
+++ orgInit.sh
@@ -1,5 +1,5 @@
 sfdx shane:org:create -f config/project-scratch-def.json -d 30 -s --wait 60 --userprefix external -o services.demo
 
-## sfdx shane:user:permset:assign -l User -g User -n datamask
+sfdx shane:user:permset:assign -l User -g User -n Contact-Bank Admin
 sfdx force:source:push
 sfdx force:org:open -p /lightning/o/Contact/list?filterName=AllContacts
